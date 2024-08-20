e3b3ad213c0b76aabd7f61065add63e0aa7b4dbc
DiegoCuesta <diegof.cuesta@farmatodo.com>
2024-08-20 12:28:58 -0500

    chore: upate name pub and change import files

--- test/kustomer_native_plugin_test.dart
+++ test/kustomer_native_plugin_test.dart
@@ -1,7 +1,5 @@
+import 'package:apps_kustomer_native_plugin/kustomer.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kustomer_native_plugin/kustomer_native_plugin_platform_interface.dart';
-import 'package:kustomer_native_plugin/kustomer_native_plugin_method_channel.dart';
-import 'package:kustomer_native_plugin/model/kustomer_config.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockKustomerNativePluginPlatform
